866e645a8aca27adc5ef81569a45124bbf14b080
staraneelpatra <staraneelpatra@github.com>
2018-03-02 09:19:41 +0530

    MongoDB init

---
+++ MongoCsharpDriver/MongoCsharpDriver/BookStore.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MongoDB.Bson;
+namespace MongoCsharpDriver
+{
+    public class BookStore
+    {
+        
+            //public ObjectId Id { get; set; }
+            public string BookTitle { get; set; }
+            public string Auther { get; set; }
+            public string Category { get; set; }
+            public string ISBN { get; set; }
+
+    }
+}
---
+++ MongoCsharpDriver/MongoCsharpDriver/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace MongoCsharpDriver
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            MongoClient client = new MongoClient();
+            var db = client.GetDatabase("LibraryDB");
+            var collection = db.GetCollection<BookStore>("BookStore");
+            BookStore bookStore = new BookStore
+            {
+                BookTitle = "MongoDB Basics",
+                ISBN = "8767687689898yu",
+                Auther = "Tanya",
+                Category = "NoSQL DBMS"
+            };
+            for (int i = 0; i < 5; i++)
+            {
+                collection.InsertOne(bookStore);
+            }
+            Console.WriteLine("inserted");
+        }
+    }
+}
