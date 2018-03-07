3a1f8db9b7b144ed6a71b92340218b9fe07b1065
staraneelpatra <staraneelpatra@github.com>
2018-03-07 22:14:04 +0530

    FindCount

--- MongoCsharpDriver/MongoCsharpDriver/Find.cs
+++ MongoCsharpDriver/MongoCsharpDriver/Find.cs
@@ -1,10 +1,24 @@
 ﻿using System;
+using MongoDB.Driver;
+using MongoDB.Bson;
 namespace MongoCsharpDriver
 {
     public class Find
     {
-        public void FindData()
+        public void FindCount()
         {
+
+            MongoClient client = new MongoClient();
+            var db = client.GetDatabase("BankDB");
+            var collection = db.GetCollection<BookStore>("BankData");
+
+            //Count Number of Documents
+            var count = collection.Count(new BsonDocument());
+            Console.Write("Count = "+count);
+
+
+
+
         }
     }
 }
--- MongoCsharpDriver/MongoCsharpDriver/Program.cs
+++ MongoCsharpDriver/MongoCsharpDriver/Program.cs
@@ -7,34 +7,12 @@ namespace MongoCsharpDriver
     class MainClass
     {
         public static void Main(string[] args)
-        {
-            MongoClient client = new MongoClient();
-            var db = client.GetDatabase("LibraryDB");
-            var collection = db.GetCollection<BookStore>("BookStore");
-            var collection1 = db.GetCollection<BsonDocument>("Data");
-            BookStore bookStore = new BookStore
-            {
-                BookTitle = "MongoDB Basics",
-                ISBN = "8767687689898yu",
-                Auther = "Tanya",
-                Category = "NoSQL DBMS"
-            };
-            collection.InsertOne(bookStore);
+        {   
+            Insert insert = new Insert();
+           // insert.InsertData();
 
-            var doc = new BsonDocument()
-            {
-                { "name", "MongoDB" },
-                { "type", "Database" },
-                { "count", 1 },
-                { "info", new BsonDocument
-                    {
-                        { "x", 203 },
-                        { "y", 102 }
-                    }}
-               };
-           collection1.InsertOne(doc);
-
-            Console.WriteLine("inserted");
+            Find find = new Find();
+            find.FindCount();
         }
     }
 }
